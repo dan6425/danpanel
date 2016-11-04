ce729c7d00216b472f533eb7990e52b0e3480dff
Dan Lewis <danl@jsbbq.local>
2016-11-04 12:09:54 -0500

    updated positioning

--- danpanel/lua/autorun/sh.lua
+++ danpanel/lua/autorun/sh.lua
@@ -17,171 +17,171 @@ if (CLIENT) then
 			draw.RoundedBox(0,0,0, w, h, Color ( 0, 255, 0, 150))
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Agree" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 50 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "agree" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			
+		DermaButton:SetText( "Agree" )				
+		DermaButton:SetPos( 20, 50 )				
+		DermaButton:SetSize( 100, 30 )				
+		DermaButton.DoClick = function()			
+		RunConsoleCommand( "act", "agree" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Beckon" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 90 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "becon" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Beckon" )				 
+		DermaButton:SetPos( 20, 90 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "becon" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Cheer" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 130 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "cheer" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Cheer" )				 
+		DermaButton:SetPos( 20, 130 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "cheer" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Dance" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 170 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "dance" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Dance" )				 
+		DermaButton:SetPos( 20, 170 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "dance" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Disagree" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 210 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "disagree" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Disagree" )				 
+		DermaButton:SetPos( 20, 210 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "disagree" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Forward" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 250 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "forward" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Forward" )				 
+		DermaButton:SetPos( 20, 250 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "forward" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Kill Me" )				 // Set the text on the button
-		DermaButton:SetPos( 20, 290 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "kill" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Kill Me" )				 
+		DermaButton:SetPos( 20, 290 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "kill" )		
 		RunConsoleCommand( "say", "RIP" )
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Laugh" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 50 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "laugh" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Laugh" )				 
+		DermaButton:SetPos( 135, 50 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "laugh" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Muscle" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 90 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "muscle" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Muscle" )				 
+		DermaButton:SetPos( 135, 90 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "muscle" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Pers" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 130 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "pers" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Pers" )				 
+		DermaButton:SetPos( 135, 130 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "pers" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Robot" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 170 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "robot" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Robot" )				 
+		DermaButton:SetPos( 135, 170 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "robot" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Salute" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 210 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "salute" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Salute" )				 
+		DermaButton:SetPos( 135, 210 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "salute" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Wave" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 250 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "wave" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Wave" )				 
+		DermaButton:SetPos( 135, 250 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "wave" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
 		end
 		
-			local DermaButton = vgui.Create( "DButton" )	// Create the button
-		DermaButton:SetParent( DermaPanel )			 // Set its parent to the panel
-		DermaButton:SetText( "Zombie" )				 // Set the text on the button
-		DermaButton:SetPos( 135, 290 )					// Set the position on the frame
-		DermaButton:SetSize( 100, 30 )				 // Set the size
-		DermaButton.DoClick = function()			 // A custom function run when clicked ( note: it a . instead of : )
-		RunConsoleCommand( "act", "zombie" )		// Run the console command "say hi" when you click it ( command, args )
+			local DermaButton = vgui.Create( "DButton" )	
+		DermaButton:SetParent( DermaPanel )			 
+		DermaButton:SetText( "Zombie" )				 
+		DermaButton:SetPos( 135, 290 )					
+		DermaButton:SetSize( 100, 30 )				 
+		DermaButton.DoClick = function()			 
+		RunConsoleCommand( "act", "zombie" )		
 		gui.EnableScreenClicker( false )
 		DermaPanel:Remove()
 		
